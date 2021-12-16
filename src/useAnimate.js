58b3fef1e6c919e418104a6ad4c9071794e51759
Captainistz <mynameisjeff7614@gmail.com>
2021-12-16 23:22:09 +0700

    🛠 edit visibility

--- src/useAnimate.js
+++ src/useAnimate.js
@@ -1,14 +1,13 @@
 import { useState, useRef, useEffect } from 'react'
 
 export default function useAnimate(ref) {
-  const [animate, setAnimate] = useState(false);
+  const [animate, setAnimate] = useState(false)
   const observerRef = useRef(null)
 
   useEffect(() => {
-    observerRef.current = new IntersectionObserver(entries => {
-      if (entries.some(entry => entry.isIntersecting))
-          setAnimate(true)
-  })
+    observerRef.current = new IntersectionObserver((entries) => {
+      if (entries.some((entry) => entry.isIntersecting)) setAnimate(true)
+    })
   }, [])
 
   useEffect(() => {
